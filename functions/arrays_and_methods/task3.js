470f7878f611463b50aed1c8a3e3ab679ddf8b79
kumar <gajmerk17@gmail.com>
2026-01-06 22:56:02 +0545

    jan-6 night

---
+++ functions/arrays_and_methods/task3.js
@@ -0,0 +1,8 @@
+// Task 3: Find Maximum in an Array
+
+// Write a function findMax(arr) that returns the largest number in the array
+
+function findMax(arr) {
+  return Math.max(...arr);
+}
+console.log(findMax([1, 2, 3, 4]));
