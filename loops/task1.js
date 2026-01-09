a6f68dd06c802f95a9674447cfe387867aa85578
kumar <gajmerk17@gmail.com>
2026-01-09 19:10:53 +0545

    jan-9 evening

---
+++ loops/task1.js
@@ -0,0 +1,7 @@
+// Task 1: Sum of First N Natural Numbers
+
+// Write a function sumOfN(n) that returns the sum of the first n natural numbers
+function sumOfN(n) {
+  return (n * (n + 1)) / 2;
+}
+console.log(sumOfN(100));
