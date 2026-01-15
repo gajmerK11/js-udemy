f82ee7d93aa6e33e4fe0160ad0fef6a57078a755
kumar <gajmerk17@gmail.com>
2026-01-15 06:58:38 +0545

    jan-15 morning

--- higher_order_functions_and_arrow_functions/task3.js
+++ higher_order_functions_and_arrow_functions/task3.js
@@ -1,3 +1,22 @@
 // Task 3: Sum of Positive Numbers
 
 // Write a function sumPositiveNumbers(arr) that takes an array of numbers and returns the sum of all positive numbers using filter() and reduce() with arrow functions
+// function positiveNumbers(element) {
+//   if (element > 0) {
+//     return element;
+//   }
+// }
+// function sumPositiveNumbers(arr) {
+//   const newArr = arr.filter(positiveNumbers);
+//   const sum = newArr.reduce((acc, current) => {
+//     return acc + current;
+//   });
+//   return sum;
+// }
+// console.log(sumPositiveNumbers([1, -1, 2]));
+const sumPositiveNumbers = (arr) => {
+  return arr
+    .filter((element) => element > 0)
+    .reduce((acc, current) => acc + current, 0);
+};
+console.log(sumPositiveNumbers([1, 0, -1]));
---
+++ higher_order_functions_and_arrow_functions/task4.js
@@ -0,0 +1,3 @@
+// Task 4: Transform Array of Objects
+
+// Write a function getNames(arr) that takes an array of objects where each object has a name property, and returns an array of just the names using map() and arrow functions
