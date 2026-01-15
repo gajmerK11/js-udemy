dacbde75144938f03f1a6cb81f161408058e01ea
kumar <gajmerk17@gmail.com>
2026-01-15 07:11:00 +0545

    jan-15 morning

--- higher_order_functions_and_arrow_functions/task4.js
+++ higher_order_functions_and_arrow_functions/task4.js
@@ -1,3 +1,14 @@
 // Task 4: Transform Array of Objects
 
 // Write a function getNames(arr) that takes an array of objects where each object has a name property, and returns an array of just the names using map() and arrow functions
+const getNames = (arr) => {
+  const a = arr.map((e) => e.name);
+  return a;
+};
+console.log(
+  getNames([
+    { name: "Kumar", role: "Team Lead" },
+    { name: "Kshitiz", role: "Senior Dev" },
+    { name: "Birat", role: "Intern" },
+  ])
+);
