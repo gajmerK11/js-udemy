efc917d2fc4bceb189b2b63074088cbe85910aad
kumar <gajmerk17@gmail.com>
2026-03-08 21:51:53 +0545

    oop

---
+++ oop-master.js
@@ -0,0 +1,24 @@
+class Vehicle {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  start() {
+    return `${this.model} is a car from ${this.make}`;
+  }
+}
+
+class Car extends Vehicle {
+  drive() {
+    return `${this.make} : This is an example of inheritance.`;
+  }
+
+  start() {
+    const original = super.start();
+    return original + " Engine running.";
+  }
+}
+
+const myNewCar = new Car("Tata", "Camry");
+console.log(myNewCar.start());
