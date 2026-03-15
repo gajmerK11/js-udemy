a4bcf53949a5f1d87374deef1fae9d0bc9b945f9
kumar <gajmerk17@gmail.com>
2026-03-15 19:47:10 +0545

    oop-challenge-3

---
+++ index-5.js
@@ -0,0 +1,24 @@
+class Shape {
+  area() {
+    return 0;
+  }
+}
+class Circle extends Shape {
+  constructor(radius) {
+    super();
+    this.radius = radius;
+  }
+  area() {
+    return Math.PI * this.radius ** 2;
+  }
+}
+class Rectangle extends Shape {
+  constructor(width, height) {
+    super();
+    this.width = width;
+    this.height = height;
+  }
+  area() {
+    return this.width * this.height;
+  }
+}
