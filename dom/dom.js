d5da88c89e90a378162102ba935d597742a9a2aa
kumar <gajmerk17@gmail.com>
2026-03-28 08:09:45 +0545

    dom

--- dom/dom.js
+++ dom/dom.js
@@ -1,9 +1,13 @@
 const changedText = document.getElementById("myParagraph");
 const btn = document.getElementById("changeTextButton");
 
+const secondBtn = document.getElementById("highlightFirstCity");
+
 const originalText = changedText.textContent;
 
 let flag = true;
+
+let colors = ["red", "blue", "green", "yellow"];
 
 btn.addEventListener("click", function () {
   if (flag) {
@@ -12,4 +16,19 @@ btn.addEventListener("click", function () {
     changedText.innerHTML = originalText;
   }
   flag = !flag;
+});
+
+let clickCounter = 0;
+
+secondBtn.addEventListener("click", function () {
+  const citiesList = document.querySelectorAll("#citiesList li");
+
+  citiesList.forEach((item) => {
+    item.style.cssText = "";
+  });
+
+  citiesList[clickCounter].style.cssText =
+    `background-color: ${colors[clickCounter]}`;
+
+  clickCounter = (clickCounter + 1) % citiesList.length;
 });
