50bbdccea576bdb82631968b5e2eba6a75d7978a
kumar <gajmerk17@gmail.com>
2026-04-12 09:35:10 +0545

    modules and commonJS

---
+++ advanced_js/app.js
@@ -0,0 +1,10 @@
+// Module import file
+
+// default import
+import multiply from "./modules.js";
+
+// named import
+import { add, subtract } from "./modules.js";
+
+console.log(multiply(2, 2));
+console.log(add(2, 2));
