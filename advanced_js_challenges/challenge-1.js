0e5b512589e984566290695eecbcf719e0061401
kumar <gajmerk17@gmail.com>
2026-04-12 19:41:02 +0545

    advanced_js_challenges

--- advanced_js_challenges/challenge-1.js
+++ advanced_js_challenges/challenge-1.js
@@ -1,7 +1,30 @@
+// Task-1
 function simulateAsyncTask() {
   console.log("Task started");
   setTimeout(() => {
     console.log("Task finished");
   }, 2000);
 }
-simulateAsyncTask();
+// simulateAsyncTask();
+
+function simulateMultipleTasks() {
+  setTimeout(() => {
+    console.log("Task 1 finished");
+  }, 1000);
+  setTimeout(() => {
+    console.log("Task 2 finished");
+  }, 2000);
+  setTimeout(() => {
+    console.log("Task 3 finished");
+  }, 3000);
+}
+// simulateMultipleTasks();
+
+function fetchDataWithCallback(callback) {
+  setTimeout(() => {
+    callback("Fetched data");
+  }, 2000);
+}
+fetchDataWithCallback(function (data) {
+  console.log(data);
+});
