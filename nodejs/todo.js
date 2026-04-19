1d804d6ccad0cef52cc7c63d9a8189488c7c394e
kumar <gajmerk17@gmail.com>
2026-04-19 21:51:14 +0545

    learning node

--- nodejs/todo.js
+++ nodejs/todo.js
@@ -1,6 +1,28 @@
 const fs = require("fs");
 const filePath = "./tasks.json";
 
+const loadTasks = () => {
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveTasks = (tasks) => {
+  const dataJSON = JSON.stringify(tasks);
+  fs.writeFileSync(filePath, dataJSON);
+};
+
+const addTask = (task) => {
+  const tasks = loadTasks();
+  tasks.push(task);
+  saveTasks(tasks);
+  console.log("Task added ", task);
+};
+
 const command = process.argv[2];
 const argument = process.argv[3];
 
