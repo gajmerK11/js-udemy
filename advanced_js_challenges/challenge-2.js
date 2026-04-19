1f48ad61debca0de7de60512b0441489f56ba374
kumar <gajmerk17@gmail.com>
2026-04-19 18:38:04 +0545

    day-13

--- advanced_js_challenges/challenge-2.js
+++ advanced_js_challenges/challenge-2.js
@@ -10,4 +10,18 @@ function createCounter() {
 }
 const increment = createCounter();
 console.log(increment());
-console.log(increment());
+
+function rateLimiter(fn, limit) {
+  let lastCallTime = null;
+
+  return function () {
+    const currentTime = Date.now();
+
+    if (lastCallTime === null || currentTime - lastCallTime >= limit) {
+      lastCallTime = currentTime;
+      return fn();
+    } else {
+      return "Rate limit exceeded";
+    }
+  };
+}
